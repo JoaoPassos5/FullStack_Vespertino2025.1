03082e6628c433b7d699ba1bb45e6d3779bffd85
JoaoPassos5 <jovifepassos@gmail.com>
2025-04-05 00:03:50 -0300

    Canvas1

--- LAB_1/Canvas/script.js
+++ LAB_1/Canvas/script.js
@@ -23,9 +23,9 @@ function desenhar_linha(x1, y1, x2, y2, cor) {
     ctx.stroke();
 }
 
-function escrever(texto, x, y, cor, tamanho, fonte) {
+function escrever(texto, x, y, cor, tamanho) {
     ctx.fillStyle = cor;
-    ctx.font = `${tamanho}px ${fonte}`;
+    ctx.font = ctx.font.replace(/\d+px/, `${tamanho}px`); 
     ctx.fillText(texto, x, y);
 }
 
@@ -34,15 +34,37 @@ desenhar_quadrado(250,0,50,50,'red');
 
 desenhar_quadrado(265,270,35,35,'black');
 desenhar_quadrado(265,240,35,35,'black');
-desenhar_quadrado(230,270,35,35,'black');
+desenhar_quadrado(235,270,30,35,'black');
 
-desenhar_quadrado(0,260,40,40,'yellow');
-desenhar_quadrado(0,220,40,40,'yellow');
-desenhar_quadrado(40,260,40,40,'yellow');
+desenhar_quadrado(0,270,30,35,'yellow');
+desenhar_quadrado(0,240,35,35,'yellow');
+desenhar_quadrado(30,270,35,35,'yellow');
+
+desenhar_quadrado(270,136,30,30,'cyan'); //QUADRADO CIANO DIREITA
+desenhar_quadrado(0,122,30,55,'cyan'); //QUADRADO CIANO ESQUERDA
 
 desenhar_linha(0,150,400,150,'green');
 
-desenhar_arco(150,300,40,1,0,'cyan','green')
+desenhar_linha(150,150,150,300,'#323232'); //LINHA CINZA
+
+desenhar_quadrado(110,150,40,40,'red');
+
+desenhar_arco(220,220,14,0,2,'yellow','green');//CIRCULO AMARELO DIREITO
+desenhar_arco(70,220,14,0,2,'yellow','green');//CIRCULO AMARELO ESQUERDO
+
+desenhar_arco(150,300,75,1,1.5,'white','green');
+
+desenhar_arco(150,150,60,1,0,'white','green');//ARCO ESQUERDO
+desenhar_arco(150,300,60,1.5,0,'white','green');//ARCO DIREITO
+desenhar_arco(150,300,40,1,0,'cyan','green');//ARCO CENTRAL
+
+desenhar_linha(300,0,150,150,'red');
+desenhar_linha(0,0,150,150,'blue');
+
+desenhar_arco(150,120,14,0,2,'cyan','blue');
+
+escrever("Canvas",115,50,'#323232',20);
+
 
 
 
