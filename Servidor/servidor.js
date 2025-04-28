2a22ad0d9cf05054493d03bacb3c55513bf61e58
JoaoPassos5 <jovifepassos@gmail.com>
2025-04-28 16:48:13 -0300

    PORFAVOR

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -2,15 +2,19 @@ let bodyParser = require("body-parser")
 require("colors");
 var http = require("http");
 var express = require("express");
+var path = require("path");
+
 
 
 var app = express();
 app.use(express.static('./public'))
 
+
 app.use(bodyParser.urlencoded({extended: false }))
 app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.set('views', './views');
+
 
 var server = http.createServer(app);
 server.listen(80);
@@ -45,3 +49,30 @@ app.get("/for_ejs", function(requisicao, resposta){
     let valor = requisicao.query.valor;
     resposta.render("exemplo_for",{valor});
 })
+
+app.post("/cadastra", function(requisicao, resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nascimento;
+
+    
+    resposta.render('resposta1',{nome , login, senha, nasc});
+
+
+})
+
+app.post("/login", function(requisicao, resposta){
+    resposta.redirect("login.html")
+});
+
+app.post('/login', (requisicao, resposta) => {
+    const { login, senha } = requisicao.body;
+
+    
+    if (login === usuarioCorreto && senha === senhaCorreta) {
+        resposta.render('resposta2', { loginValido: true, usuario: login });
+    } else {
+        resposta.render('resposta2', { loginValido: false });
+    }
+});
